aeca85baa5538d9b60b520cbdb5c9a115959a05e
Ben Robertson <benjamingrobertson@me.com>
2019-05-01 17:57:46 -0400

    Initial commit.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import Table from './table';
 
-import styles from './styles.css'
+// import './styles.css'
 
 export default class ExampleComponent extends Component {
   static propTypes = {
@@ -9,14 +10,17 @@ export default class ExampleComponent extends Component {
   }
 
   render() {
-    const {
-      text
-    } = this.props
+    const headers = ['Band', 'Singer', 'Inception', 'Label'];
 
+    const rows = [
+      ['Napalm Death', 'Barney Greenway', '1981', 'Century Media'],
+      ['Carcass', 'Jeff Walker', '1985', 'Earache'],
+      ['Extreme Noise Terror', 'Dean Jones', '1985', 'Candlelight'],
+      ['Discordance Axis', 'Jon Chang', '1992', 'Hydrahead']
+    ];
     return (
-      <div className={styles.test}>
-        Example Component: {text}
+      <div className="App">
+        <Table rows={rows} headers={headers} rowHeaders caption="Grindcore bands" sortable />
       </div>
-    )
-  }
+);}
 }
